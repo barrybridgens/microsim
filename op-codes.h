6d4706f897245ba6ae74ba25685000feb349b0ef
Barry Bridgens <barry@bridgens.me.uk>
2020-05-15 20:51:42 +0100

    Initial op-code definitions

---
+++ op-codes.h
@@ -0,0 +1,7 @@
+/* Microsim - op-code definitions */
+
+#define LDA_I    1   /* Load accumulator immediate - data: value to load */
+#define LDA_M    2   /* Load accumulator from memory - data: memory address high, memory address low */
+#define STA      3   /* Store accumulator to memory - data: memory address high, memory address low */
+#define ADD_I    4   /* Add immediate - data: value to add to accumulator */
+#define ADD_M    5   /* Add from memory - data: memory address high, memory address low */
